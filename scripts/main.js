7821b30f995094e942c6257b585bfbb097bf50e5
stevenremenapp <remenapp@gmail.com>
2018-11-19 20:05:51 -0500

    DRY out .scss file, add JS to name animation so animation completes on hover

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,14 @@
 // document.getElementById('hero-left').addEventListener('mouseover', function(event) {
 //     console.log(event.target);
 // });
+
+let nameLetter = document.getElementsByClassName('name-letter');
+
+for (let i = 0; i < nameLetter.length; i++) {
+    nameLetter[i].addEventListener('mouseover', function() {
+        nameLetter[i].classList.add('animatedLetter');
+    });
+    nameLetter[i].addEventListener('animationend', function() {
+        nameLetter[i].classList.remove('animatedLetter');
+    });
+}
