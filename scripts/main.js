8c65814c58fc1b39ad5c949745330c99396e76fe
stevenremenapp <remenapp@gmail.com>
2018-11-29 11:05:13 -0500

    Add basic menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+// NAME ANIMATION
+
 // document.getElementById('hero-left').addEventListener('mouseover', function(event) {
 //     console.log(event.target);
 // });
@@ -12,3 +14,31 @@ for (let i = 0; i < nameLetter.length; i++) {
         nameLetter[i].classList.remove('animatedLetter');
     });
 }
+
+// MENU
+
+let menuBtn = document.querySelector('.menu-icon');
+let menuCloseBtn = document.querySelector('.menu-closed-icon');
+let menu = document.querySelector('.menu');
+let overlay = document.querySelector('.menu-overlay');
+
+menuBtn.addEventListener('click', function() {
+    menuBtn.style.display = "none";
+    menuCloseBtn.style.display = "block";
+    menu.style.display = "flex";
+    overlay.style.display = "block";
+});
+
+menuCloseBtn.addEventListener('click', function() {
+    menuBtn.style.display = "block";
+    menuCloseBtn.style.display = "none";
+    menu.style.display = "none";
+    overlay.style.display = "none";
+});
+
+overlay.addEventListener('click', function() {
+    menuBtn.style.display = "block";
+    menuCloseBtn.style.display = "none";
+    menu.style.display = "none";
+    overlay.style.display = "none";
+});
